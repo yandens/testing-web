bc331b3e4d4b1b0be119e6dc9ada02e6f75b1e7b
Wayan Deden Setyawan <dedensetyawan17120104@gmail.com>
2023-12-24 21:26:08 +0800

    update code

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -131,7 +131,7 @@
                     </div>
 
                     <div class="ml-4 text-center text-sm text-gray-500 dark:text-gray-400 sm:text-right sm:ml-0">
-                        Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+                        Laravel version{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                     </div>
                 </div>
             </div>
